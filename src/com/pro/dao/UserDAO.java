53cf030f04cea7c7232b773d06e87b8b49e44453
CUG-LZT <cug_lzt@163.com>
2018-07-03 00:08:53 +0800

    add to log4j

--- src/com/pro/dao/UserDAO.java
+++ src/com/pro/dao/UserDAO.java
@@ -15,14 +15,18 @@ import com.pro.entity.User;
  * 此类用于写和User表的所有操作
  */
 public class UserDAO {
-	public List<User> queryUserList(){
+	public List<User> queryUserList(String username,String role){
 		DBaccess dBaccess = new DBaccess();
 		SqlSession sqlSession = null;
 		List<User> userlist = new ArrayList<User>();
+		//因为mybatis只允许传递一个参数，所以用对象类型去封装两个参数
+		User user = new User();
+		user.setUsername(username);
+		user.setRole(role);
 		try {
 			sqlSession = dBaccess.getSqlSession();
 			//通过实体配置文件的命名空间和sql的id；sqlSession执行sql语句
-			userlist = sqlSession.selectList("User.queryUserList");
+			userlist = sqlSession.selectList("User.queryUserList",user);
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
--- src/com/pro/test/Test.java
+++ src/com/pro/test/Test.java
@@ -8,7 +8,9 @@ import com.pro.entity.User;
 public class Test {
 	public static void main(String[] args) {
 		UserDAO userDAO = new UserDAO();
-		List<User> userlist = userDAO.queryUserList();
+		//String username="1",role="1";
+		String username="",role="1";
+		List<User> userlist = userDAO.queryUserList( username , role);
 		for (User user : userlist) {
 			System.out.println(user.toString());
 		}
